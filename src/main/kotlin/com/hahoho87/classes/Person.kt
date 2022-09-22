40cf8a0c64a734d2544ee08bebcf5b9429f26f21
bky4564 <bky4564@hunet.co.kr>
2022-09-22 13:02:33 +0900

    add kotlin constructor

--- src/main/kotlin/com/hahoho87/classes/Person.kt
+++ src/main/kotlin/com/hahoho87/classes/Person.kt
@@ -1,6 +1,9 @@
 package com.hahoho87.classes
 
-class Person {
+class Person(
+    val name: String = "",
+    val age: Int = 0
+) {
     fun action() {
        println("Person walks")
     }
@@ -9,4 +12,9 @@ class Person {
 fun main() {
     val person = Person()
     person.action()
+    println("Name: ${person.name}, Age: ${person.age}")
+
+    val person1 = Person("Hahoho87", 30)
+    person1.action()
+    println("Name: ${person1.name}, Age: ${person1.age}")
 }
