d0580327af60fe55f95fa4228671cbe9f9693957
bky4564 <bky4564@hunet.co.kr>
2022-10-13 13:01:42 +0900

    add kotlin custom getter and setter

--- src/main/kotlin/com/hahoho87/classes/Item.kt
+++ src/main/kotlin/com/hahoho87/classes/Item.kt
@@ -2,6 +2,20 @@ package com.hahoho87.classes
 
 class Item() {
     var name: String = ""
+    var price: Double = 0.0
+
+    get() {
+        println("Inside Getter")
+        return field
+    }
+    set(value) {
+        if (value >= 0.0) {
+            println("Inside Setter")
+            field = value
+        } else {
+            throw IllegalArgumentException("Negative Price is not Allowed")
+        }
+    }
 
     constructor(_name: String) : this() {
         this.name = _name
@@ -11,4 +25,7 @@ class Item() {
 fun main() {
     val item = Item("item1")
     println(item.name)
+    item.price = 10.0
+    println(item.price)
+    item.price = -10.0
 }
