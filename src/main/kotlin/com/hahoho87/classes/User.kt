e2e2e96e263b7be03d445f57ee7f9af6714d3c8d
bky4564 <bky4564@hunet.co.kr>
2022-10-17 13:14:07 +0900

    feat: add kotlin companion object keyword

--- src/main/kotlin/com/hahoho87/classes/User.kt
+++ src/main/kotlin/com/hahoho87/classes/User.kt
@@ -1,6 +1,6 @@
 package com.hahoho87.classes
 
-open class User(val name: String){
+open class User(val name: String) {
     open var isLoggedIn: Boolean = false
     open fun login() {
         println("Inside user login")
@@ -9,18 +9,27 @@ open class User(val name: String){
 
 class Student(name: String) : User(name) {
     override var isLoggedIn: Boolean = false
+
+    companion object {
+        const val numberOfEnroll = 10
+        fun country() = "Korea"
+    }
+
     override fun login() {
         super.login()
         println("Inside student login")
     }
 }
 
-class Instructor(name: String): User(name)
+class Instructor(name: String) : User(name)
 
 fun main() {
     val student = Student("John")
     println("name is ${student.name}")
     student.login()
+    val country = Student.country()
+    println("Country is $country")
+    println("NumberOfEnrollment is : ${Student.numberOfEnroll}")
 
     val instructor = Instructor("Jane")
     println("name is ${instructor.name}")
