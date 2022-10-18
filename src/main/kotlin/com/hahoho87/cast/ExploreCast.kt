e892a5013b10fe4d163517396977764ce4dc41da
bky4564 <bky4564@hunet.co.kr>
2022-10-18 13:13:31 +0900

    feat: type checking, casting and smart casting

---
+++ src/main/kotlin/com/hahoho87/cast/ExploreCast.kt
@@ -0,0 +1,33 @@
+package com.hahoho87.cast
+
+import com.hahoho87.classes.Course
+
+fun castNumber(any: Any) {
+    when (any) {
+        any as Double -> println("Value is Double")
+    }
+}
+
+fun main() {
+    val course = Course(1, "Kotlin", "Hahoho")
+    val test = "test"
+    checkType(course)
+    checkType(test)
+
+    castNumber(1.0)
+//    castNumber(1) // ClassCastException
+
+    val number = 1
+    val numberDouble = number.toDouble()
+    println(numberDouble)
+
+    castNumber(numberDouble)
+
+}
+
+fun checkType(type: Any) {
+    when (type) {
+        is Course -> println(type)
+        is String -> println(type.lowercase())
+    }
+}
