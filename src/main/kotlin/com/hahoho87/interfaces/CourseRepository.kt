832d979c8d50d71e9fb29c22c9f9c41deb179a42
bky4564 <bky4564@hunet.co.kr>
2022-10-18 12:54:56 +0900

    feat: Defining and Overriding Variables

--- src/main/kotlin/com/hahoho87/interfaces/CourseRepository.kt
+++ src/main/kotlin/com/hahoho87/interfaces/CourseRepository.kt
@@ -3,6 +3,9 @@ package com.hahoho87.interfaces
 import com.hahoho87.classes.Course
 
 interface CourseRepository {
+
+    val isCoursePersisted: Boolean
+
     fun getById(id: Int): Course
 
     fun save(course: Course): Int {
@@ -32,12 +35,22 @@ class AB : A, B {
 }
 
 class SqlCourseRepository : CourseRepository {
+    override var isCoursePersisted: Boolean = false
     override fun getById(id: Int): Course {
         return Course(id, "Kotlin", "hahoho")
+    }
+
+    override fun save(course: Course): Int {
+        println("course in SqlCourseRepository : $course")
+        isCoursePersisted = true
+        return super.save(course)
     }
 }
 
 class NoSqlCourseRepository : CourseRepository {
+    override val isCoursePersisted: Boolean
+        get() = TODO("Not yet implemented")
+
     override fun getById(id: Int): Course {
         return Course(id, "Kotlin", "hahoho")
     }
@@ -54,13 +67,15 @@ fun main() {
     println("course id is ${course.id}, course name is ${course.name}, course author is ${course.author}")
     val savedId = sqlCourseRepository.save(Course(5, "Kotlin", "hahoho"))
     println("course id is $savedId")
-
+    println("isCoursePersisted is ${sqlCourseRepository.isCoursePersisted}")
+    println()
 
     val noSqlCourseRepository = NoSqlCourseRepository()
     val noSqlCourse = noSqlCourseRepository.getById(2)
     println("course id is ${noSqlCourse.id}, course name is ${noSqlCourse.name}, course author is ${noSqlCourse.author}")
     val noSqlId = noSqlCourseRepository.save(Course(6, "Kotlin", "hahoho"))
     println("course id is $noSqlId")
+    println()
 
     val ab = AB()
     ab.doSomething()
